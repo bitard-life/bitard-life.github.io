b034fae772bfd3cdcf392abd26707882d5b4973e
Bitard <aleksandr.n.petrov@gmail.com>
2021-04-15 04:46:31 +0300

    Add files via upload

---
+++ js/scene_menu.js
@@ -0,0 +1,196 @@
+// [ Сцена: игровое меню ]
+scenes.menu = {
+    //Ресурсы сцены -------------------------------------------------------------------------------
+    resources: {
+        disclaimer:     { type: 'image', src: '/res/disclaimer.png?v=0.1' },
+        rain:           { type: 'image', src: '/res/postproc/rain.png?v=0.1' },
+        menu_btn:       { type: 'image', src: '/res/menu/btn.png?v=0.1' },
+        auto:           { type: 'image', src: '/res/final/auto.png?v=0.1' },
+        dorogi:         { type: 'image', src: '/res/final/dorogi.png?v=0.1' },
+        krisha:         { type: 'image', src: '/res/final/krisha.png?v=0.1' },
+        luji:           { type: 'image', src: '/res/final/luji.png?v=0.1' },
+        anon:           { type: 'image', src: '/res/menu/anon.png?v=0.1' },
+        objects:        { type: 'image', src: '/res/final/objects.png?v=0.1' },
+        sound_bg:       { type: 'sound', mp3: '/res/menu/5P4C3_C4173T.mp3?v=0.1', ogg: '/res/menu/5P4C3_C4173T.ogg?v=0.1' }
+    },
+    
+    //Слои сцены ----------------------------------------------------------------------------------
+    layers: [],
+    
+    //Объекты сцены -------------------------------------------------------------------------------
+    objects: {
+        //Дисклеймер (для получения клика на запуск звука)
+        disclaimer: {
+            //Добавляем объект на сцену
+            add: function() {
+                if( config.debug ) console.log( '[menu.disclaimer] add' );
+                
+                //Сохраняем координаты клика
+                tmp.menu.click_x = game.cursor.click_x;
+                tmp.menu.click_y = game.cursor.click_y;
+                
+                //Включаем отрисовку объекта
+                scenes.menu.layers.push( 'disclaimer' );
+            },
+            
+            //Отрисовываем
+            draw: function() {
+                 //Рисуем дисклеймер 
+                game.canvas.context.drawImage( scenes.menu.resources.disclaimer.data, 0, 0 );
+                
+                //Кликнули
+                if( tmp.menu.click_x != game.cursor.click_x  &&  tmp.menu.click_y != game.cursor.click_y ) {
+                    tmp.menu.click_x = Math.ceil( game.cursor.click_x / game.canvas.scale );
+                    tmp.menu.click_y = Math.ceil( game.cursor.click_y / game.canvas.scale );
+                    
+                    if( tmp.menu.click_x > 250 && tmp.menu.click_x < 390 && tmp.menu.click_y > 300 && tmp.menu.click_y < 350 ) {
+                        //Удаляем чейнджлог со сцены
+                        scenes.menu.objects.disclaimer.del();
+                    }
+                    
+                    tmp.menu.click_x = game.cursor.click_x;
+                    tmp.menu.click_y = game.cursor.click_y;
+                } 
+            },
+            
+            //Удаляем объект со сцены
+            del: function() {
+                if( config.debug ) console.log( '[menu.disclaimer] del' );
+                
+                //Прекращаем отрисовку объекта
+                let obj_pos = scenes.menu.layers.indexOf( 'disclaimer' );
+                if( obj_pos > -1 ) scenes.menu.layers.splice( obj_pos, 1 );
+                
+                //Добавляем меню на сцену
+                scenes.menu.objects.menu.add();
+            }
+        },
+        //Дороги
+        menu: {
+            //Добавляем объект на сцену
+            add: function() {
+                if( config.debug ) console.log( '[menu.menu] add' );
+                
+                //Включаем отрисовку объекта
+                scenes.menu.layers.push( 'menu' );
+                
+                //Запускаем фоновый звук
+                scenes.menu.resources.sound_bg.data.loop = true;
+                scenes.menu.resources.sound_bg.data.start();
+                
+                //Запускаем дождь
+                tmp.menu.rain = 0;
+                game.postproc = function() {
+                    tmp.menu.rain += 20;
+                    if ( tmp.menu.rain > 200 ) tmp.menu.rain = 0;
+                    game.canvas.context.drawImage( scenes.menu.resources.rain.data, 0, 200 - tmp.menu.rain, 640, 360, 0, 0, 640, 360 );
+                };
+                
+                //Настраиваем движение машин [ x, y, тип авто ]
+                tmp.menu.auto = [ [40,0,0], [349,0,1], [20,0,2], [250,0,3] ];
+                tmp.menu.x = 600;
+                tmp.menu.y = 330;
+            },
+            
+            auto: function() {
+                for( let i = 0; i<4; i++) {
+                    let x = tmp.menu.auto[ i ][ 0 ];
+                    x += ( tmp.menu.auto[ i ][ 2 ] > 1 ? -1 : 1 );
+                    
+                    //Движение закончилось
+                    if( x == 380 || x == 0 ) {
+                        //Сбрасываем координату
+                        x = ( tmp.menu.auto[ i ][ 2 ] > 1 ? 380 - Math.round(Math.random()) * 6 : 0 + Math.round(Math.random()) * 6 );
+                        //Ставим случайную тачку
+                        tmp.menu.auto[ i ][ 2 ] = Math.round(Math.random()) + ( tmp.menu.auto[ i ][ 2 ] > 1 ? 2 : 0 );
+                    }
+                    
+                    //Обновляем координаты
+                    tmp.menu.auto[ i ][ 0 ] = x;
+                    tmp.menu.auto[ i ][ 1 ] = 520 - Math.ceil( x * 0.75 );
+                }
+            },
+            
+            //Отрисовываем объекты в меню
+            draw: function() {
+                //Получаем короткие ссылки
+                let context = game.canvas.context;
+                let tmp_lob = tmp.menu;
+                let res = scenes.menu.resources;
+                
+                context.fillStyle = '#000000';
+                context.fillRect( 0, 0, game.canvas.width, game.canvas.height );
+                
+                context.drawImage( res.dorogi.data, 155, 62, 640, 360, 0, 0, 640, 360 );
+                
+                //Костыль движения авто
+                scenes.menu.objects.menu.auto();
+                for( let x = 0; x < 4; x++ ) {
+                    context.drawImage(
+                        res.auto.data,
+                        tmp.menu.auto[ x ][ 2 ] * 50, 0,
+                        50, 44,
+                        666 - tmp.menu.auto[ x ][ 0 ],
+                        tmp.menu.auto[ x ][ 1 ] + ( tmp.menu.auto[ x ][ 2 ] > 1 ? 20 : -10 ) - 280,
+                        50, 44
+                    );
+                }
+                
+                
+                context.drawImage( res.objects.data, 155-31, 62+56, 640, 360, 0, 0, 640, 360 );
+                context.drawImage( res.krisha.data,  155+121, 62-256, 640, 360, 0, 0, 640, 360 );
+                context.drawImage( res.luji.data,  155-250, 62-328, 640, 360, 0, 0, 640, 360 );
+                context.drawImage( res.anon.data,  155-464, 62-282, 640, 360, 0, 0, 640, 360 );
+            },
+            
+            //Удаляем объект со сцены
+            del: function() {
+                if( config.debug ) console.log( '[menu.menu] del' );
+                
+                //Прекращаем отрисовку объекта
+                let obj_pos = scenes.menu.layers.indexOf( 'menu' );
+                if( obj_pos > -1 ) scenes.menu.layers.splice( obj_pos, 1 );
+                
+                //Выключаем фоновый звук
+                scenes.menu.resources.sound_bg.data.stop();
+                
+                //Отключаем дождь
+                game.postproc = scenes.empty;
+            }
+        }
+    },
+    //Создание сцены ------------------------------------------------------------------------------
+    enable: function() {
+        if( config.debug ) console.log( '[menu] scene enable' );
+        //Создаем временные переменные для сцены
+        tmp.menu = {};
+        
+        //Запускаем отрисовку сцены загрузки
+        game.draw = scenes.loading;
+        
+        //Загружаем ресурсы сцены в опративную память
+        LoadSceneMemory( scenes.menu.resources, function() {
+            //Добавляем чейнджлог на сцену
+            scenes.menu.objects.disclaimer.add();
+            
+            //Запускаем отрисовку сцены
+            game.draw = scenes.menu.update;
+        } );
+    },
+    
+    //Обновление сцены ----------------------------------------------------------------------------
+    update: function() {
+        let context = game.canvas.context;
+        
+        context.clearRect( 0, 0, game.canvas.width, game.canvas.height );
+        
+        //Рисуем объекты сцены
+        for( let i = 0; i < scenes.menu.layers.length; i++ ) {
+            scenes.menu.objects[ scenes.menu.layers[ i ] ].draw();
+        }
+    },
+    
+    //Выгрузка сцены из памяти --------------------------------------------------------------------
+    disable: function() {
+    }
+};
---
+++ js/scene_preloader.js
@@ -0,0 +1,304 @@
+// [ Сцена: загрузка файлов ]
+scenes.preloader = {
+    //Ресурсы сцены -------------------------------------------------------------------------------
+    resources: {
+        logo: { type: 'image64', file: 
+            'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPkAAACJCAMAAADZs25jAAAAUVBMVEUAAAAAAAAA'+
+            'ADMzAABmAACZAAAAMwAAMzMzMwAzMzNmMwBmMzOZMwDMMwD/MwBmZmaZZgCZZjPMZgDMZjP/ZgD/ZjOZmZn/mQ'+
+            'D/mTPMzMz///9mBTHqAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAt6'+
+            'SURBVHja7V2JYps4EBWb7q7lOlZLQKD8/4cuuq/RBbLjbJHrgjFgpDfvzWgkCEI7C0ssz3KWs5zlLGc5y+sVVu'+
+            'nHWWH/r/p8lrOcpW/MfsbwZznL9+fy3jfq9HnP8izPUPaz/F/QfZV3jQacpbcV1KLSo2d4ltfw47nXa2rCWfpw'+
+            'myWWbcp8BMUT1ef48ZZXC5r7LOgLOP4HGFw74ujLVP5JEPyP0O7D7a965S3tIET5poh/6o/i+FcgnEe8a1PUOb'+
+            'Jvz/XEiwHvl7OKLui3GfzznNcDPEyR4y+Neleut4Uyj0e9Q1zBWjgu0YXKt+B7YxOFzUARhsuXODa4qr05LlDF'+
+            'Q7q8JuKdOJ6r+PAiiLPOii4QJ0O2ULkfFdZPNPfFRyLXybYithP9Pf+KKsIQcywV3znbiVwh3jURsYnvjNQZOT'+
+            'iY9gmtvJ8aCjW3e3HjIFIXXIvQS6L35ivIsRq9htXeajs1q8S5Htfa9BmxOLavE9OQj9O4lfdxHsdpnrYV/llh'+
+            'LpCS6GNedfEBU0pkQ4hP/Fy6YcSKrVZ8rL+d6OOl2PJPWBCQiGYgstlxP7nz2vg6ybKI/5Fc/nQkjqrWl1joC9'+
+            'GfLJoY6QuV31HgWHe7tiDsXg8yzYHtMZ05rnV9nNdpWnnV1w1yWT6UhUpeK/T5Qhiz5DO/dvW/QlPvqw8IjmXB'+
+            'dqkhAlpm1tXOWO5CTNMe9Od+M0hbv6nKbpVf1lU0wTR62u7iFm6liqkuO8UnCtuLu93anY+5PidnOFHfHlB4AH'+
+            'Vj6+tkDF6sLldX2TUnmWKwRkiuSkywFiuhR4qvmyi7xzK7nYhVeR5sGlN+T4j+DRpKIOrUBMrWDcM5xydh76NV'+
+            'Ywc3bBnMkJUereDYYbIRdO/YcLuNKCig7a569Es4aaYN+KqrrXC3FR9MDC+hMpKs/bplI9XmwKRxUusGHTmPtl'+
+            'u9oJbnG+ZyD22VNB3asH2+XFzOZbEk5//N3ACuri9XmA8uwmarYjihvBEUw+O9bCjsb0/x3BIAm+86oi7k7fKx'+
+            'GLhXDfv4r6omC3iuUWAmFqByyXWICJ8ttykTd44djD93tjPDbZ9WzIRF2A+hD0RvKIje3iaJ8rRs71niP0YxO3'+
+            'UswFyUwY86HMdBZOdqxJDGPPDndqnP1yf3Jousnqi0wnqZhb1PP/4arNv1/LlAS9LSLnVUoH061sJAjIYrK8Dh'+
+            'dh1SEOb48+0rolQmwnynP2cxy39zlO/TfdF4b83wjgc/ePAxl1UWGu8pMnZ8ehifU8Nmf7sKVKG4XZ37MM9jXy'+
+            '5Pr81cu/PpPk9G152cHNbRnOqVSTdrvseKs1TH7rYLSPWxQgP97dJEiM38aEscMLFNQfp0Wv1e+Wi9GYd8WTZ/'+
+            'Huo6/KKJzzSzL93Vc6d7Ek5pxLUr1z58XQ3yhqtgTu6Z74pUBWs2eiVvsrKzZvg8T+vlubmYByahwNybY+s6Wl'+
+            'cC90v2jT/LKLCG0Zocqqxwvr5NoHoqy13H6qZIW/9En98RbVaZbR1t59SIu2TB51Yy+ff6d3g86831Cs67TSA9'+
+            'y8dduHIZr6t4ZoRScansO8GIvCrPE6iL+vyUSRiRh9nc2Sqi9rGQh3XRwE5sv4fjHZS92ZeLarxLZRNgz8rm13'+
+            'zNhzgGHMjrcj1SXhlcX5dFBukK+oWn4pbxlh138KM6k7xIcpwSoqlCeBKdQpohdpQF0ILjI6sBVlilGpfJkTle'+
+            'bj8vmt55zIujMASX7UbF7a6ogNrRxZ8To+urrvBdYK4+rLa/LuyAl/eY58QMH0A8pxnTIWa/5E561KYT190MiU'+
+            'iwo6226+Qhv47Gv9tODBohtPwxGD/SxnVqkdsJ70E86c9NtnWwRj5v1b6LpeL8yrussj22pfD5b3+5oTxyevc6'+
+            'O+NxFhcGrIiXIMjul+F7Nde9dr7Z9LK3NjmsF7IvXN2PIeqvO8YeXdJQrlHViB6cktir8JJaF5N444txHcdxXt'+
+            '8nz/BF523k31/h/roxdh91XK4Ptplbl9oEx8fijjzPXdLWaUVS70bVNrNJylF4BLYdcS9bH6Mb1N706XdyvfbC'+
+            'RtVrXXS9N9/ncRNWduu/h5pC1f4A9b1sjmdVh/15fp7Ab6cLo7Qdg5FaQtmrKg5hDp89jhKP+PMhb+4m864i2q'+
+            's7omrfsLLjSshjngfnp9DkhSM8l02AeeQUvWX3bVb9de3Prnk24yzHt2hMjJOjTbwQoNeZiUg0wfUj+XZw/tvW'+
+            'HVfZKd11kSs3d3gNUHbi9rt9oDCNfoVuYTxxPnuY+/13/2SYVXGd1ebb3dcnMcYu/bjQuA9rnhXK3jynKjv5jI'+
+            'Jn26Hw2VyZpuh1seOp3O7fgjgbOSMuobIjAnI5907zPDof6+TToZfssutkhfiHU8EEBlSXtc+iK0w4hKP4Y7k4'+
+            'FMfaYRL6rtUNA/1pCCkKeOeAu+ES4DkCYgYbQe5HPT3TUPzKD91RFUy//lNCA6d8MOuCOYNi/U7ZmcA/85B1lp'+
+            'n32QlaIzQcY7fb/dCD7eQ5gn5HEYj1H3pQKP6DnIlC0yUzulZQdtQJcwRNPe5574qm1MXmIpbVindW2Q1niet9'+
+            'YW5HJcNzxkKiR1maxtg90RTSp80iC8vrv1yivEiYzgqUHaeDzd2Y053qXiVtrspuln5f7nc7GW5g4BgY6INzvp'+
+            'l6pXiMua4gjmOdJn9H8dKbcGUc9fsty1cwZs/gh1MZtiaeD92U3eW5Cl119mmERlRYOB+euBaR4SyUaCnyPI7d'+
+            'h/wQxE6u64neIikxvucVGlbwZAIJSgiQA5j3mi+DLJ9mPcR0CebAQCM0Lu/CCK7cZ8dZfw7zfOjgz+E57tynie'+
+            '7pFedHTgZwPK0yv+Lh15/nrC12l7DcRMx6n35DmVYgZvd7YyFnrR8Ha17HcwKN6fWcF6dCV9k/+yj55ARKyeQR'+
+            '2Y85GY710Yuxu/Zp3NTf/3WzowDPSSI2B8fN7Lw7QmLOFv05fow/D+5Xu8mI/a04Jl7Os4eR3yBGR2k75hiKAn'+
+            'rey2LnzMxvcKYVxXzOZVAoFLN/tvN8SHRTD08i8dCax3VafmSi9ayy+24NFxGswjyVeu42/1Vc0RsfPr/BvjWe'+
+            'R2eVHUH9czj6wzU8d/vnocD18eeOwouJy7/s9IDsPaNpjHApD9eM+YEIjlVlZcx4eqzMyXtYnZgdubF/GJm7Bb'+
+            'fwPIra6bGxNcjwqeyaz+slLSQRbqWsGaQVrZjj9AyKHvez2ADumh7NCOfEA5nQMD/u3gPTwvM485MY7Ojgz5VP'+
+            'Q1Dbw3c0ptR/yKPeiDnk0lBHxFVlpr+j+W61yh734uKse0RbkOee/8fZmL1HVkZm4MZUtI5qYnaQmcn7yqswH5'+
+            'K+vBvqcsKQF4snM7V5Ywf7ZfxRA7yQoT5uh2ZN9H2gk3PPWpTdhDu4cK/Jac66ORP6lxKVoxnEUTd/TkA1blV2'+
+            '1DhrAsfz4YweRHNGafV8ONas7AkfHTZZwdgTXC88u8LRBEpInMHBXZ8rA4Yf5ftIcMq3pqyoaT5cftJg72fKMJ'+
+            'TKeEBE8XU43QEuwl6tCuRI5JbPymDX9+YRZ1XGjiomvlL/GRfZ6bFH57wWZ0QWboUrKztCrBJ3XDUtcXOGfQ08'+
+            '8eAkXDMAWD0tQMXd8cR+8Rwhrx8u7niA9iqg3aEJarOalpU4uR/kOAkim2ITNTOu7U7XrhyPLm8DBpO6uwNxWd'+
+            'nhOTho9/1u/Z8PByOEMmiaquOSB3jBB6O1XSICuixE8rTl1XTP6Rc8C7J8qtqfbX9M9sOfM/scw/yGTwBtQ/04'+
+            'oVACefTIJwunw4/SE/hLXRr0cKS/SWl5qu8DHlfdNJ5eZZCtUvW45zqz74l6y1N/X+SS6v4qR/8n9iN0/p2WV0'+
+            'QbPY3bJ/q9PUDN9z3/EucXyd0fhip7GudRw1/hYOjQX+X5D547SqxLgMhiAAAAAElFTkSuQmCC'
+        }
+    },
+    //Слои сцены ----------------------------------------------------------------------------------
+    layers: [],
+    
+    //Объекты сцены -------------------------------------------------------------------------------
+    objects: {
+        //Полоса прогресса закачки
+        progressbar: {
+            //Добавляем объект на сцену
+            add: function() {
+                if( config.debug ) console.log( '[preloader.pogressbar] add' );
+                scenes.preloader.layers.push( 'progressbar' );
+            },
+            
+            //Отрисовываем объект
+            draw: function() {
+                //Получаем короткие ссылки
+                let context = game.canvas.context;
+                let tmp_pre = tmp.preloader;
+                let res = scenes.preloader.resources;
+                    
+                //Очищаем холст
+                context.fillStyle = '#888888';
+                context.clearRect( 0, 0, game.canvas.width, game.canvas.height );
+                
+                //Рисуем лого
+                let dx = Math.ceil( ( game.canvas.width / 2) - ( res.logo.data.width / 2) );
+                let dy = Math.ceil( ( game.canvas.height / 2) - ( res.logo.data.height / 2 ) - 10 );
+                context.drawImage( res.logo.data, dx, dy );
+                
+                //Рисуем контур прогрессбара
+                let barW = Math.ceil( ( game.canvas.width / 100) * 80 );
+                let barX = Math.ceil( ( game.canvas.width - barW ) / 2 );
+                let barY = Math.ceil( game.canvas.height  / 2 ) + ( res.logo.data.height / 2 ) - 10;
+                context.fillStyle = '#222222';
+                context.fillRect( barX, barY, barW, 20 );
+                    
+                //Рисуем полосу прогресса
+                if( tmp_pre.bytes_load > 0 ) {
+                    //Вычисляем, сколько процентов загружено
+                    let precentage = Math.ceil( 100 / ( tmp_pre.bytes_all / tmp_pre.bytes_load ) );
+                    
+                    //Рисуем полосу
+                    context.fillStyle = '#888888';
+                    context.fillRect(
+                        barX + 5,
+                        barY + 5,
+                        ( ( barW - 10 ) / 100 ) * precentage,
+                        10
+                    );
+                }
+            },
+            
+            //Удаляем объект со сцены
+            del: function() {
+                if( config.debug ) console.log( '[preloader.pogressbar] del' );
+                let obj_pos = scenes.preloader.layers.indexOf( 'progressbar' );
+                if( obj_pos > -1 ) scenes.preloader.layers.splice( obj_pos, 1 );
+            }
+        }
+    },
+    
+    //Создание сцены ------------------------------------------------------------------------------
+    enable: function() {
+        if( config.debug ) console.log( '[preloader] scene enable' );
+        
+        //Создаем временные переменные для сцены
+        let tmp_pre = tmp.preloader = {};
+        
+        //Ограничиваем сцену до ~4 fps
+        tmp_pre.prev_fps = config.fps_max;
+        config.fps_max = 4;
+        
+        //Список ресурсов к скачиванию: [ { src: '/../xxx.xxx', ref: [ { scene: 'scene_name', name: 'res_name' }, ... ] }, ... ]
+        tmp_pre.resources = [],
+        
+        //Ищем ресурсы во всех сценах
+        tmp_pre.scene_names = Object.getOwnPropertyNames( scenes );
+        for( let s = 0; s < tmp_pre.scene_names.length; s++ ) {
+            //У сцены нашлись ресурсы на скачивание
+            if( scenes[ tmp_pre.scene_names[ s ] ].resources !== undefined ) {
+                //Перебираем ресурсы и заносим их в список для скачивания
+                let scene_res = scenes[ tmp_pre.scene_names[ s ] ].resources;
+                let scene_res_names = Object.getOwnPropertyNames( scene_res );
+                for( let r = 0; r < scene_res_names.length; r++ ) {
+                    let scene_resource = scene_res[ scene_res_names[ r ] ];
+                    
+                    //Поверяем, загружен ли уже ресурс
+                    if( scene_resource.file !== undefined ) break;
+                    
+                    //Для звука переключаем на поддерживаемый формат
+                    if( scene_resource.type == 'sound' ) scene_resource.src = ( game.mp3_sound ? scene_resource.mp3 : scene_resource.ogg );
+                    
+                    //Поверяем, есть ли уже в списке на закачку такой ресурс
+                    let res_find = false;
+                    for( let t = 0; t < tmp_pre.resources.length; t++ ) {
+                        if( tmp_pre.resources[ t ].src == scene_resource.src ) {
+                            //Сохраняем ссылку на ресурс для этой сцены (случай с одним ресурсом на несколько сцен)
+                            tmp_pre.resources[ t ].ref.push( { scene: tmp_pre.scene_names[ s ], name: scene_res_names[ r ] } );
+                            res_find = true;
+                            break;
+                        }
+                    }
+                    
+                    if( res_find === false ) {
+                        //Добавляем ресурс в список на закачку
+                        tmp_pre.resources.push( { src: scene_resource.src, ref:[ { scene: tmp_pre.scene_names[ s ], name: scene_res_names[ r ] } ] } );
+                    }
+                }
+            }
+        }
+        
+        //Количество файлов к скачиванию
+        tmp_pre.files_count = tmp_pre.resources.length;
+        
+        //Файлов чекнуто через HEAD
+        tmp_pre.files_checked = 0;
+        
+        //Файлов загружено
+        tmp_pre.files_load = 0;
+        
+        //Общее количество байт на скачивание
+        tmp_pre.bytes_all = 0;
+        
+        //Байт скачано
+        tmp_pre.bytes_load = 0;
+        
+        //Загружаем ресурсы сцены в оперативную память
+        LoadSceneMemory( scenes.preloader.resources, function() {
+            //Добавляем прогрессбар на сцену
+            scenes.preloader.objects.progressbar.add();
+            
+            //Запускаем отрисовку сцены
+            game.draw = scenes.preloader.update;
+        } );
+        
+        //Оцениваем размеры ресурсных файлов, через http-заголовок HEAD
+        for( let i = 0; i < tmp_pre.files_count; i++ ) {
+            let xhr = new XMLHttpRequest();
+            xhr.open( 'HEAD', tmp_pre.resources[ i ].src, true );
+            xhr.onreadystatechange = function() {
+                if( this.readyState == this.DONE ) {
+                    //Получаем и сохраняем размер файла
+                    let file_size = parseInt( xhr.getResponseHeader( 'Content-Length' ) );
+                    tmp_pre.resources[ i ].size = file_size;
+                    
+                    //Увеличиваем счетчик общего количества байт для скачивания
+                    tmp_pre.bytes_all += file_size;
+                    
+                    //Увеличиваем счетчик чекнутых файлов
+                    tmp_pre.files_checked++;
+                    
+                    if( config.debug ) console.log( '[preloader] file checked ' + tmp_pre.files_checked + '/' + tmp_pre.files_count + ': ' + tmp_pre.resources[ i ].src );
+                }
+            };
+            xhr.send();
+        }
+        
+        //Ждем завершения оценки размеров файлов
+        let timerHead = setInterval( function() {
+            if( tmp_pre.files_count  === tmp_pre.files_checked ) {
+                clearInterval( timerHead );
+                
+                //Начинаем скачивать файлы ресурсов
+                for( let j = 0; j < tmp_pre.files_count; j++ ) {
+                    let xhr = new XMLHttpRequest();
+                    xhr.open( 'GET', tmp_pre.resources[ j ].src, true );
+                    xhr.responseType = 'arraybuffer';
+                    //if( tmp_pre.resources[ j ].src.substr(-3) == 'mp3' || tmp_pre.resources[ j ].src.substr(-3) == 'ogg' ) xhr.responseType = 'blob';
+                    
+                     //Сохраняем в объекте запроса количество скачанных байт
+                    xhr.loaded = 0;
+                    
+                    xhr.onprogress = function( e ) {
+                        //Обновляем общее количество скачанных байт
+                        tmp_pre.bytes_load += e.loaded - this.loaded;
+                        this.loaded = e.loaded;
+                    };
+                    
+                    xhr.onload = function( e ) {
+                        //На случай кеширования файлов
+                        tmp_pre.bytes_load += tmp_pre.resources[ j ].size - this.loaded;
+                        
+                        //Сохраняем массив байт в объект ресурсов сцены
+                        for( let k = 0; k < tmp_pre.resources[ j ].ref.length; k++ ) {
+                            let scene_name = tmp_pre.resources[ j ].ref[ k ].scene;
+                            let res_name = tmp_pre.resources[ j ].ref[ k ].name;
+                            
+                            scenes[ scene_name ].resources[ res_name ].file = this.response;
+                        }
+                        
+                        //Увеличиваем счетчик загруженных файлов
+                        tmp_pre.files_load++;
+                        
+                        if( config.debug ) console.log( '[preloader] file loaded ' + tmp_pre.files_load + '/' + tmp_pre.files_count + ': ' + tmp_pre.resources[ j ].src );
+                    };
+                    
+                    xhr.send();
+                }
+            }
+        }, 500 );
+    },
+    
+    //Обновление сцены ----------------------------------------------------------------------------
+    update: function() {
+        let tmp_pre = tmp.preloader;
+        
+        //Рисуем объекты сцены
+        for( let i = 0; i < scenes.preloader.layers.length; i++ ) {
+            scenes.preloader.objects[ scenes.preloader.layers[ i ] ].draw();
+        }
+        
+        //Файлы загрузились
+        if( tmp_pre.files_load == tmp_pre.files_count ) {
+            //Завершаем текущую сцену
+            scenes.preloader.disable();
+            
+            //Запускаем сцену лобби
+            scenes.menu.enable();
+        }
+    },
+    
+    //Выгрузка сцены из памяти --------------------------------------------------------------------
+    disable: function() {
+        //Выключаем отрисовку сцены
+        game.draw = scenes.empty;
+        
+        //Возвращаем прежний fps
+        config.fps_max = tmp.preloader.prev_fps;
+        
+        //Удаляем прогрессбар со сцены
+        scenes.preloader.objects.progressbar.del();
+        
+        //Выгружаем ресурсы сцены из опративной памяти
+        FreeSceneMemory( scenes.preloader.resources );
+        
+        //Удаляем созданные переменные
+        let tmp_pre = tmp.preloader;
+        delete tmp_pre.prev_fps;
+        delete tmp_pre.res_names;
+        delete tmp_pre.files_count;
+        delete tmp_pre.files_checked;
+        delete tmp_pre.files_load;
+        delete tmp_pre.bytes_all;
+        delete tmp_pre.bytes_load;
+        
+        if( config.debug ) console.log( '[preloader] scene disable' );
+    }
+};
