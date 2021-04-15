cb77c0e71538278ead9a4d3341adc4a82a3791a4
bitard <aleksandr.n.petrov@gmail.com>
2021-04-15 21:19:48 +0300

    fix sound

--- js/scene_menu.js
+++ js/scene_menu.js
@@ -12,7 +12,7 @@ scenes.menu = {
         anon:           { type: 'image', src: '/res/menu/anon.png?v=0.1' },
         objects:        { type: 'image', src: '/res/final/objects.png?v=0.1' },
         sound_bg:       { type: 'sound', mp3: '/res/menu/5P4C3_C4173T.mp3?v=0.1', ogg: '/res/menu/5P4C3_C4173T.ogg?v=0.1' },
-        sound_step:     { type: 'sound', mp3: '/res/final/step_dkiller2204.mp3?v=0.1', ogg: '/res/final/step_dkiller2204.ogg?v=0.1' }
+       //sound_step:     { type: 'sound', mp3: '/res/final/step_dkiller2204.mp3?v=0.1', ogg: '/res/final/step_dkiller2204.ogg?v=0.1' }
     },
     
     //Слои сцены ----------------------------------------------------------------------------------
